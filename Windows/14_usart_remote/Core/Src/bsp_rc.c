38cb13f7297d04115578f9b52bcf3eee8fd11852
wbk <242778877@qq.com>
2025-01-02 10:33:32 +0800

    Add Windows version code with initial implementation

---
+++ Windows/14_usart_remote/Core/Src/bsp_rc.c
@@ -0,0 +1,41 @@
+#include "bsp_rc.h"
+#include "main.h"
+
+extern UART_HandleTypeDef huart3;
+extern DMA_HandleTypeDef hdma_usart3_rx;
+
+void RC_init(uint8_t *rx1_buf, uint8_t *rx2_buf, uint16_t dma_buf_num)
+{
+    //enable the DMA transfer for the receiver request
+    SET_BIT(huart3.Instance->CR3, USART_CR3_DMAR);
+
+    //enalbe idle interrupt
+    __HAL_UART_ENABLE_IT(&huart3, UART_IT_IDLE);
+
+    //disable DMA
+    __HAL_DMA_DISABLE(&hdma_usart3_rx);
+    while(hdma_usart3_rx.Instance->CR & DMA_SxCR_EN)
+    {
+        __HAL_DMA_DISABLE(&hdma_usart3_rx);
+    }
+
+    hdma_usart3_rx.Instance->PAR = (uint32_t) & (USART3->DR);
+    //memory buffer 1
+    hdma_usart3_rx.Instance->M0AR = (uint32_t)(rx1_buf);
+    //memory buffer 2
+    hdma_usart3_rx.Instance->M1AR = (uint32_t)(rx2_buf);
+    //data length
+    hdma_usart3_rx.Instance->NDTR = dma_buf_num;
+    //enable double memory buffer
+    SET_BIT(hdma_usart3_rx.Instance->CR, DMA_SxCR_DBM);
+
+    //enable DMA
+    __HAL_DMA_ENABLE(&hdma_usart3_rx);
+
+}
+
+
+
+
+
+

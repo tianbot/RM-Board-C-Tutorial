d3fcdaeae67f29bdeff208b88f775f50205888d2
wbk <242778877@qq.com>
2025-01-07 17:35:44 +0800

    feat:Add the Chassis_control features of the Windows version

---
+++ Windows/24_Chassis_control/Chassis/can.h
@@ -0,0 +1,191 @@
+/**
+  ******************************************************************************
+  * File Name          : CAN.h
+  * Description        : This file provides code for the configuration
+  *                      of the CAN instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __can_H
+#define __can_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+#include "cmsis_os.h"
+#include "main.h"
+#include "detect_task.h"
+
+/* USER CODE END Includes */
+
+extern CAN_HandleTypeDef hcan1;
+extern CAN_HandleTypeDef hcan2;
+
+/* USER CODE BEGIN Private defines */
+#define CHASSIS_CAN hcan1
+#define GIMBAL_CAN hcan2
+/* USER CODE END Private defines */
+
+void MX_CAN1_Init(void);
+void MX_CAN2_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* CAN filter init */
+extern void can_filter_init(void);
+
+/* CAN send and receive ID */
+typedef enum
+{
+    CAN_CHASSIS_ALL_ID = 0x200,
+    CAN_3508_M1_ID = 0x201,
+    CAN_3508_M2_ID = 0x202,
+    CAN_3508_M3_ID = 0x203,
+    CAN_3508_M4_ID = 0x204,
+
+    CAN_YAW_MOTOR_ID = 0x205,
+    CAN_PIT_MOTOR_ID = 0x206,
+    CAN_TRIGGER_MOTOR_ID = 0x207,
+    CAN_GIMBAL_ALL_ID = 0x1FF,
+
+} can_msg_id_e;
+
+//rm motor data
+typedef struct
+{
+    uint16_t ecd;
+    int16_t speed_rpm;
+    int16_t given_current;
+    uint8_t temperate;
+    int16_t last_ecd;
+} motor_measure_t;
+
+
+/**
+  * @brief          send control current of motor (0x205, 0x206, 0x207, 0x208)
+  * @param[in]      yaw: (0x205) 6020 motor control current, range [-30000,30000] 
+  * @param[in]      pitch: (0x206) 6020 motor control current, range [-30000,30000]
+  * @param[in]      shoot: (0x207) 2006 motor control current, range [-10000,10000]
+  * @param[in]      rev: (0x208) reserve motor control current
+  * @retval         none
+  */
+/**
+  * @brief          发送电机控制电流(0x205,0x206,0x207,0x208)
+  * @param[in]      yaw: (0x205) 6020电机控制电流, 范围 [-30000,30000]
+  * @param[in]      pitch: (0x206) 6020电机控制电流, 范围 [-30000,30000]
+  * @param[in]      shoot: (0x207) 2006电机控制电流, 范围 [-10000,10000]
+  * @param[in]      rev: (0x208) 保留，电机控制电流
+  * @retval         none
+  */
+extern void CAN_cmd_gimbal(int16_t yaw, int16_t pitch, int16_t shoot, int16_t rev);
+
+/**
+  * @brief          send CAN packet of ID 0x700, it will set chassis motor 3508 to quick ID setting
+  * @param[in]      none
+  * @retval         none
+  */
+/**
+  * @brief          发送ID为0x700的CAN包,它会设置3508电机进入快速设置ID
+  * @param[in]      none
+  * @retval         none
+  */
+extern void CAN_cmd_chassis_reset_ID(void);
+
+/**
+  * @brief          send control current of motor (0x201, 0x202, 0x203, 0x204)
+  * @param[in]      motor1: (0x201) 3508 motor control current, range [-16384,16384] 
+  * @param[in]      motor2: (0x202) 3508 motor control current, range [-16384,16384] 
+  * @param[in]      motor3: (0x203) 3508 motor control current, range [-16384,16384] 
+  * @param[in]      motor4: (0x204) 3508 motor control current, range [-16384,16384] 
+  * @retval         none
+  */
+/**
+  * @brief          发送电机控制电流(0x201,0x202,0x203,0x204)
+  * @param[in]      motor1: (0x201) 3508电机控制电流, 范围 [-16384,16384]
+  * @param[in]      motor2: (0x202) 3508电机控制电流, 范围 [-16384,16384]
+  * @param[in]      motor3: (0x203) 3508电机控制电流, 范围 [-16384,16384]
+  * @param[in]      motor4: (0x204) 3508电机控制电流, 范围 [-16384,16384]
+  * @retval         none
+  */
+extern void CAN_cmd_chassis(int16_t motor1, int16_t motor2, int16_t motor3, int16_t motor4);
+
+/**
+  * @brief          return the yaw 6020 motor data point
+  * @param[in]      none
+  * @retval         motor data point
+  */
+/**
+  * @brief          返回yaw 6020电机数据指针
+  * @param[in]      none
+  * @retval         电机数据指针
+  */
+extern const motor_measure_t *get_yaw_gimbal_motor_measure_point(void);
+
+/**
+  * @brief          return the pitch 6020 motor data point
+  * @param[in]      none
+  * @retval         motor data point
+  */
+/**
+  * @brief          返回pitch 6020电机数据指针
+  * @param[in]      none
+  * @retval         电机数据指针
+  */
+extern const motor_measure_t *get_pitch_gimbal_motor_measure_point(void);
+
+/**
+  * @brief          return the trigger 2006 motor data point
+  * @param[in]      none
+  * @retval         motor data point
+  */
+/**
+  * @brief          返回拨弹电机 2006电机数据指针
+  * @param[in]      none
+  * @retval         电机数据指针
+  */
+extern const motor_measure_t *get_trigger_motor_measure_point(void);
+
+/**
+  * @brief          return the chassis 3508 motor data point
+  * @param[in]      i: motor number,range [0,3]
+  * @retval         motor data point
+  */
+/**
+  * @brief          返回底盘电机 3508电机数据指针
+  * @param[in]      i: 电机编号,范围[0,3]
+  * @retval         电机数据指针
+  */
+extern const motor_measure_t *get_chassis_motor_measure_point(uint8_t i);
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ can_H */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
